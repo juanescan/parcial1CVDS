5a4da33ba33323ad47028e1c01346ae24e17411c
juanescan <juanestebancancelado63@gmail.com>
2024-09-19 13:23:50 -0500

    avance

---
+++ producto.java
@@ -0,0 +1,15 @@
+public class producto {
+    private final String name;
+    private final int precio;
+    private final int cantidad;
+    private final String categoria;
+
+    public producto(String name,int precio,int cantidad,String categoria) {
+        this.name = name;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.categoria = categoria;
+
+
+    }
+}
